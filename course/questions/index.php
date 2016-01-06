2196d97280227ec11d504be4a289b82072dd350d
rgordonatrsgc <rgordon@rsgc.on.ca>
2016-01-06 10:58:06 +0000

    Initial questions landing page created

--- course/questions/index.php
+++ course/questions/index.php
@@ -15,18 +15,18 @@ session_start();
 if(!isset($_SESSION['username']))
 {
     // Not logged in, re-direct to the login page
-    redirect('../index.php');
+    redirect('../../index.php');
 }
 
 // This page should always be submitted with a GET request method
 // If it is not, redirect to logged in home page
 if(!isset($_GET['cid']))  {
 
-    redirect('../home.php');
+    redirect('../../home.php');
 
 } else {
     
-    // Get details for this course
+    // Get curriculum for this course
     // Connect to database
     $host = "127.0.0.1";
     $user = "rgordonatrsgc";
@@ -42,7 +42,7 @@ if(!isset($_GET['cid']))  {
     $provided_id = htmlspecialchars($_GET['cid']);
     
     // Run the query
-    $query = "SELECT id, code, url FROM course WHERE id = " . $provided_id . ";";
+    $query = "SELECT id, code FROM course WHERE id = " . $provided_id . ";";
     $result = mysqli_query($connection, $query);
     
     // Verify that a result was obtained from the database
@@ -50,63 +50,29 @@ if(!isset($_GET['cid']))  {
         
         // Something happened when talking to database, re-direct to logged-in home page
         // TODO: Implement proper error logging
-        redirect('../home.php');
+        redirect('../../home.php');
         
     } else {
+        
         if (mysqli_num_rows($result) != 1) {
             
             // This shouldn't happen either, course-id should exist and return a single row, so,
             // re-direct to logged-in home page
             // TODO: Implement proper error logging
-            redirect('../home.php');
+            redirect('../../home.php');
             
         } else {
             
             // We have a valid result for this course
             $row = mysqli_fetch_assoc($result);
             $course_code = $row['code'];
-            $course_url = $row['url'];
             $course_id = $row['id'];
-            
-            // Build query
-            $query  = "SELECT COUNT(m.id) AS expectations_count ";
-            $query .= "FROM course c ";
-            $query .= "INNER JOIN strand s ";            
-            $query .= "ON s.course_id = c.id ";            
-            $query .= "INNER JOIN overall_expectation o ";            
-            $query .= "ON o.strand_id = s.id ";            
-            $query .= "INNER JOIN minor_expectation m ";            
-            $query .= "ON m.overall_expectation_id = o.id ";            
-            $query .= "WHERE c.id = " . $course_id . ";";
-            
-            // Run query
-            $result = mysqli_query($connection, $query);
-            
-            // Check for a result
-            if ($result == false) {
-                
-                // Something happened when talking to database, re-direct to logged-in home page
-                // TODO: Implement proper error logging
-                redirect('../../home.php');
-                
-            } else {
-                
-                if (mysqli_num_rows($result) != 1) {
-            
-                    // This shouldn't happen either, query uses aggregate function and should return a single row, so,
-                    // re-direct to logged-in home page
-                    // TODO: Implement proper error logging
-                    redirect('../home.php');
-                    
-                } else {
-                    
-                    // We have a valid result for this query
-                    $row = mysqli_fetch_assoc($result);
-                    $expectations_count = $row['expectations_count'];
 
-                }
-                
-            }
+            // Run query to get questions linked to curriculum for this course
+            // TODO: Implement this (after question add page is complete)
+            //       Populate the $output variable
+            //$query = "";
+            //$result = mysqli_query($connection, $query);
 
         }
 
@@ -149,21 +115,18 @@ $csslink = $base . "css/style.css";
 
     <nav>
         <ul>
-            <li><a href="../home.php">Home</a> > <?php echo $course_code; ?></li>
+            <li><a href="../../home.php">Home</a> > <a href="../?cid=<?php echo $course_id; ?>"><?php echo $course_code; ?></a> > Questions</li>
             <li><a href="<?php echo $base; ?>logout.php">logout</a></li>
             <li><?php echo $_SESSION['username']; ?></li>
         </ul>
     </nav>
 
     <main>
+        <p><a href="./add/?cid=<?php echo $course_id; ?>">add</a></p>
         <p>
-            <ul>
-                <li><a href="./questions/?cid=<?php echo $course_id ?>">See Questions (xx so far)</a></li>
-                <li><a href="./curriculum/?cid=<?php echo $course_id ?>">See Curriculum (<?php echo $expectations_count; ?> expectations)</a></li>
-            </ul>
+    <?php echo $output; ?>
+            There are currently no questions defined for this course.
         </p>
-        
-        <p><a href="<?php echo $course_url; ?>">Canoninical Curriculum</a></p>
     </main>
 
 </body>
