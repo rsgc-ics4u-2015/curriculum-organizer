ec0a3a597f7b622f89d8099b73e8fb6a3fbf6671
Russell Gordon <rgordon@rsgc.on.ca>
2016-01-06 05:41:39 -0500

    Legibility of large queries

--- db/useful-queries.sql
+++ db/useful-queries.sql
@@ -1,30 +1,30 @@
 -- all overall expectation (titles only) for a course
-select s.id as sid, s.code as scode, s.title as stitle, o.id as oid, o.code as ocode, o.title as otitle
-from course c
-inner join strand s
-on s.course_id = c.id
-inner join overall_expectation o
-on o.strand_id = s.id
-where c.id = 1;
+SELECT s.id AS sid, s.code AS scode, s.title AS stitle, o.id AS oid, o.code AS ocode, o.title AS otitle
+FROM course c
+INNER JOIN strand s
+ON s.course_id = c.id
+INNER JOIN overall_expectation o
+ON o.strand_id = s.id
+WHERE c.id = 1;
 
 -- all minor expectations for a course
-select s.id as sid, s.code as scode, s.title as stitle, o.id as oid, o.code as ocode, o.title as otitle, m.id as mid, m.code as mcode, m.description as mdescription
-from course c
-inner join strand s
-on s.course_id = c.id
-inner join overall_expectation o
-on o.strand_id = s.id
-inner join minor_expectation m
-on m.overall_expectation_id = o.id
-where c.id = 1;
+SELECT s.id AS sid, s.code AS scode, s.title AS stitle, o.id AS oid, o.code AS ocode, o.title AS otitle, m.id AS mid, m.code AS mcode, m.description AS mdescription
+FROM course c
+WHERE strand s
+ON s.course_id = c.id
+INNER JOIN overall_expectation o
+ON o.strand_id = s.id
+INNER JOIN minor_expectation m
+ON m.overall_expectation_id = o.id
+WHERE c.id = 1;
 
 -- count of minor expectations for a course
-select count(m.id)
-from course c
-inner join strand s
-on s.course_id = c.id
-inner join overall_expectation o
-on o.strand_id = s.id
-inner join minor_expectation m
-on m.overall_expectation_id = o.id
-where c.id = 1;
+SELECT COUNT(m.id) AS expectations_count
+FROM course c
+INNER JOIN strand s
+ON s.course_id = c.id
+INNER JOIN overall_expectation o
+ON o.strand_id = s.id
+INNER JOIN minor_expectation m
+ON m.overall_expectation_id = o.id
+WHERE c.id = 1;
